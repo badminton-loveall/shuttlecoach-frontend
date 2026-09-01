7d813cce8a17913203de7e70b40e6d1741ab0271
badminton-loveall <your-badminton-email@example.com>
2026-09-01 11:17:00 +0530

    bug fixes

--- src/components/TemplatesTab.tsx
+++ src/components/TemplatesTab.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
+import { useState, useEffect, forwardRef, useImperativeHandle } from 'react';
 import apiClient from '../utils/apiClient';
 import TemplateFormModal from './TemplateFormModal';
 import '../styles/pages.css';
